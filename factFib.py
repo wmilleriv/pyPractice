eb354c8cd49c4ef886a54c9a9c92cb5aa4abbdf9
William Miller <Wmilleiv@gmail.com>
2025-02-19 10:40:38 -0500

    fizzbuzz

--- factFib.py
+++ factFib.py
@@ -1,6 +1,6 @@
 def fact(n):
     x=1
-    for i in range(1,n+1):
+    for i in range(2,n+1):
         x=x*i
     return x
 
--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,3 +1,9 @@
 for i in range(100):
-    print(i)
+    if i%3==0:
+        print("fizz", end ="")
+    if i%5==0:
+        print("buzz", end ="")
+    if i%5!=0 and i%3!=0:
+        print(i, end="")
+    print()
     
