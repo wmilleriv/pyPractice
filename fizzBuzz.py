5c3a6a9074d061da5fca5afd25ede30710b88ecd
William Miller <Wmilleiv@gmail.com>
2025-02-21 10:36:02 -0500

    add pop

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,9 +1,11 @@
-for i in range(100):
+for i in range(10000):
     if i%3==0:
         print("fizz", end ="")
     if i%5==0:
         print("buzz", end ="")
-    if i%5!=0 and i%3!=0:
+    if i%7==0:
+        print("pop", end="")
+    if i%5!=0 and i%3!=0 and i%7!=0:
         print(i, end="")
     print()
     
