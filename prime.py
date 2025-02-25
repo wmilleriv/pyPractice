f07101d13bce7edd71b5f07433d094e39d22267b
William Miller <Wmilleiv@gmail.com>
2025-02-25 11:37:43 -0500

    prime broken

--- prime.py
+++ prime.py
@@ -2,19 +2,25 @@ import sys
 import math
 
 def isPrime(n):
-    for j in range(2,int(pow(n,(1/2)))):
-        if(i%j==0):
+    if n<1:
+        return False;
+    if n<=3:
+        return True;
+    for j in range(22,int(pow(n,(1/2)))):
+        if(n%j==0):
             return False;
-    print(i);
     return True;
 
 
 
 
-#primes=[0,1,2]
-f=open("primes.txt","a")
-for i in range(100000000000):
+#f=open("./primes.txt", "a+")
+#primes=[]
+#for line in f:
+ #   primes+=f.readline
+for i in range(100):
     if(isPrime(i)):
-       f.write(i + ", ")
-   
-f.close()
+        print(i)
+      # f.write(str(i))
+#f.close()
+    
