95736be65693cdcf9bde78d2d69aba921a216ba2
William Miller <Wmilleiv@gmail.com>
2025-03-06 11:43:29 -0500

    grid works

--- graphPi.py
+++ graphPi.py
@@ -6,26 +6,29 @@ scHeight=screen.window_height()
 plane=turtle.Turtle()
 
 def drawLines(lineCount):
-    plane.setpos(800,0)
-    for i in range(int(lineCount/2)):
+    for i in range(lineCount):
         plane.down()
         plane.forward(800)
         plane.right(90)
-        plane.up()
         plane.forward(800/lineCount)
         plane.right(90)
-        plane.down()
         plane.forward(800)
         plane.left(90)
-        plane.up()
         plane.forward(800/lineCount)
         plane.left(90)
+        
         
 
 screen.screensize(800,800)
 turtle.setworldcoordinates(0,0,800,800)
-plane.speed(100)
+plane.speed(10)
 plane.left(90)
 
 drawLines(10)
+plane.up()
+plane.setpos(800,0)
+plane.left(90)
+drawLines(10)
 turtle.mainloop()
+
+
