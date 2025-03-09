71b048b62d6ed5cfe118b5d274e773a66c9117c1
William Miller <Wmilleiv@gmail.com>
2025-03-08 19:10:15 -0500

    rm label

--- graphPi.py
+++ graphPi.py
@@ -6,7 +6,7 @@ scHeight=screen.window_height()
 plane=turtle.Turtle()
 
 def drawLines(lineCount):
-    loopCount=int(lineCount/2)+1#two lines per loop
+    loopCount=int(lineCount)+1#two lines per loop
     for i in range(loopCount):
         plane.down()
         plane.forward(800)
@@ -18,22 +18,15 @@ def drawLines(lineCount):
         plane.forward(800/(lineCount))
         plane.left(90)
         
-def label():
-    plane.color("black")
-    plane.setpos(0,0)
-    plane.write(0, font=("Verdana", 12, "bold"))
-
-
-
 
 
 screen.screensize(800,800)
-turtle.setworldcoordinates(0,0,800,800)
+turtle.setworldcoordinates(0,0,100000,10)
 plane.speed(100)
 plane.left(90)
 plane.color("green")
 
-drawLines(100)
+drawLines(10)
 plane.up()
 plane.setpos(800,0)
 plane.left(90)
