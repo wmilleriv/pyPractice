0696274c05d2c4ad7b44e252d9a9e4bffd358ea1
William Miller <Wmilleiv@gmail.com>
2025-03-10 14:19:04 -0400

    pi size

--- graphPi.py
+++ graphPi.py
@@ -5,34 +5,31 @@ scWidth=screen.window_width()
 scHeight=screen.window_height()
 plane=turtle.Turtle()
 
-def drawLines(lineCount):
-    loopCount=int(lineCount)+1#two lines per loop
-    for i in range(loopCount):
+def drawYLines():
+    for i in range(6):
         plane.down()
-        plane.forward(800)
-        plane.right(90)
-        plane.forward(800/(lineCount))
-        plane.right(90)
-        plane.forward(800)
+        plane.forward(100000)
         plane.left(90)
-        plane.forward(800/(lineCount))
+        print(scHeight/5)
+        plane.forward(2)
         plane.left(90)
+        plane.forward(100000)
+        plane.right(90)
+        plane.forward(2)
+        plane.right(90)
         
 
 
 screen.screensize(800,800)
 turtle.setworldcoordinates(0,0,100000,10)
 plane.speed(100)
-plane.left(90)
 plane.color("green")
-
-drawLines(10)
+drawYLines()
 plane.up()
 plane.setpos(800,0)
-plane.left(90)
-drawLines(100)
 turtle.mainloop()
 
+drawYLines()
 k=1
 s=0
 
