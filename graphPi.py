27bbc8f4e2a7a77f73980d743e2558f8bce101f8
William Miller <Wmilleiv@gmail.com>
2025-03-12 09:57:52 -0400

    graph complete

--- graphPi.py
+++ graphPi.py
@@ -5,39 +5,27 @@ scWidth=screen.window_width()
 scHeight=screen.window_height()
 plane=turtle.Turtle()
 
-def drawYLines():
-    for i in range(6):
-        plane.down()
-        plane.forward(100000)
-        plane.left(90)
-        plane.forward(2)
-        plane.left(90)
-        plane.forward(100000)
-        plane.right(90)
-        plane.forward(2)
-        plane.right(90)
-
 def drawXLines():
-    for i in range (10):
+    for i in range(6):
+        plane.up()
+        plane.setpos(0, i*2)
         plane.down()
-        plane.forward(100)
-        plane.left(90)
-        plane.forward(2)
-        plane.left(90)
-        plane.forward(100)
-        plane.right(90)
-        plane.forward(2)
-        plane.right(90)
+        plane.forward(100000)
+
+def drawYLines():
+    plane.setheading(90)
+    for i in range (100):
+        plane.up()
+        plane.setpos(i*1000, 0)
+        plane.down()
+        plane.forward(10)
 
 screen.screensize(800,800)
 turtle.setworldcoordinates(0,0,100000,10)
 plane.speed(100)
 plane.color("green")
-drawYLines()
-plane.up()
-plane.setpos(800,0)
-plane.setheading(0)
 drawXLines()
+drawYLines()
 turtle.mainloop()
 
 k=1
