cc5377e164c4b3afcabaf430c522220fb2bb3c1b
William Miller <Wmilleiv@gmail.com>
2025-03-12 10:43:56 -0400

    complete

--- graphPi.py
+++ graphPi.py
@@ -6,9 +6,9 @@ scHeight=screen.window_height()
 plane=turtle.Turtle()
 
 def drawXLines():
-    for i in range(6):
+    for i in range(4):
         plane.up()
-        plane.setpos(0, i*2)
+        plane.setpos(0, i)
         plane.down()
         plane.forward(100000)
 
@@ -19,22 +19,36 @@ def drawYLines():
         plane.setpos(i*1000, 0)
         plane.down()
         plane.forward(10)
+def label():
+    plane.down
+    plane.setpos(99000, 3)
+    plane.color("black")
+    plane.write(3, font=("Verdana", 12, "bold"))
 
 screen.screensize(800,800)
-turtle.setworldcoordinates(0,0,100000,10)
+turtle.setworldcoordinates(0,2,100000,4)
 plane.speed(100)
 plane.color("green")
 drawXLines()
 drawYLines()
-turtle.mainloop()
+plane.up()
+label()
 
 k=1
 s=0
 
-for i in range(1000000):
+for i in range(100000):
     if(i%2==0):
         s+=4/k
     else:
         s-=4/k
-        plane.setpos(i*10,s)
-        plane.down()
+
+    k+=2
+
+    plane.setpos(i*100,s)
+    plane.color("blue")
+    plane.dot()
+    print(s)
+turtle.mainloop()
+
+
