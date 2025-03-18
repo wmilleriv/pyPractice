415c27b3aacada7514735647b2b94bbdcab33093
William Miller <Wmilleiv@gmail.com>
2025-03-18 15:53:08 -0400

    shapeLib

---
+++ shapes/main.py
@@ -0,0 +1,3 @@
+import shapeLib
+
+shapeLib.drawSquare()
---
+++ shapes/shapeLib.py
@@ -0,0 +1,12 @@
+import turtle
+
+def drawSquare():
+    square=turtle.Turtle()
+    for i in range(4):
+        square.forward(100)
+        square.right(90)
+
+def drawTriangle():
+
+def drawCircle():
+
