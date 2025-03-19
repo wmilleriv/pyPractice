99873abaaff7bee90e96f6f1e07347578651afab
William Miller <Wmilleiv@gmail.com>
2025-03-19 12:44:13 -0400

    house

--- shapes/main.py
+++ shapes/main.py
@@ -1,3 +1,24 @@
 import shapeLib
+import turtle
 
-shapeLib.drawSquare()
+turt=turtle.Turtle()
+turt.shape("turtle")
+turt.speed(1000)
+turt.fillcolor("red")
+turt.begin_fill()
+shapeLib.drawQuadrilateral(turt,-200,-250,-200,150,200,150,200,-250)
+turt.end_fill()
+turt.fillcolor("blue")
+turt.begin_fill()
+shapeLib.drawTriangle(turt,200,150,-200,150,0,250)
+turt.end_fill()
+turt.fillcolor("#8B4513")
+turt.begin_fill()
+shapeLib.drawQuadrilateral(turt,0,-250,0,-100,100,-100,100,-250)
+turt.end_fill()
+turt.fillcolor("yellow")
+turt.begin_fill()
+
+turt.end_fill()
+
+turt.screen.mainloop()
--- shapes/shapeLib.py
+++ shapes/shapeLib.py
@@ -1,12 +1,31 @@
 import turtle
+import math
 
-def drawSquare():
-    square=turtle.Turtle()
-    for i in range(4):
-        square.forward(100)
-        square.right(90)
+def drawQuadrilateral(turt,x1,y1,x2,y2,x3,y3,x4,y4):
+    turt.up()
+    turt.setpos(x1,y1)
+    turt.down()
+    turt.goto(x2,y2)
+    turt.goto(x3,y3)
+    turt.goto(x4,y4)
+    turt.goto(x1,y1)
 
-def drawTriangle():
+def drawTriangle(turt,x1,y1,x2,y2,x3,y3):
+    turt.up()
+    turt.setpos(x1,y1)
+    turt.down()
+    turt.goto(x2,y2)
+    turt.goto(x3,y3)
+    turt.goto(x1,y1)
 
-def drawCircle():
+def drawCircle(turt, radius, x, y):
+    turt.up()
+    turt.setpos(x,(y+radius))
+    turt.down()
+    for i in range(360):
+        circumference=2*radius*math.pi
+        turt.forward(circumference/360)
+        turt.right(1)
+
+
 
