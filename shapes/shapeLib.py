0a23764a6c98aa88ba676b8649b3bf42e860f88f
William Miller <Wmilleiv@gmail.com>
2025-03-24 14:30:58 -0400

    class

--- shapes/shapeLib.py
+++ shapes/shapeLib.py
@@ -1,5 +1,5 @@
 import turtle
-import math
+
 
 def drawQuadrilateral(turt,x1,y1,x2,y2,x3,y3,x4,y4):
     turt.up()
@@ -17,4 +17,7 @@ def drawTriangle(turt,x1,y1,x2,y2,x3,y3):
     turt.goto(x2,y2)
     turt.goto(x3,y3)
     turt.goto(x1,y1)
+    
+class Square()
+    def __init__(self,length, x, y):
 
