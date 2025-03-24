d601631b5c8132329afcd65683823d45362c889d
William Miller <Wmilleiv@gmail.com>
2025-03-24 11:47:35 -0400

    remove circle

--- shapes/shapeLib.py
+++ shapes/shapeLib.py
@@ -18,14 +18,3 @@ def drawTriangle(turt,x1,y1,x2,y2,x3,y3):
     turt.goto(x3,y3)
     turt.goto(x1,y1)
 
-def drawCircle(turt, radius, x, y):
-    turt.up()
-    turt.setpos(x,(y+radius))
-    turt.down()
-    for i in range(360):
-        circumference=2*radius*math.pi
-        turt.forward(circumference/360)
-        turt.right(1)
-
-
-
