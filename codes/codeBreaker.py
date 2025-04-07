9922871e39944102ca408d29393359aaae843628
William Miller <Wmilleiv@gmail.com>
2025-04-07 15:57:36 -0400

    print offset

--- codes/codeBreaker.py
+++ codes/codeBreaker.py
@@ -1,12 +1,13 @@
 f=open("TestMessage1.txt")
 out=""
-
+firstPass=[]
 data=f.read().split()
 for char in data:
     char=int(char)
     offset=0
     while(char%128!=0):
         char-=1
-    print(char)
+    firstPass.append(char)
+    print(offset)
     out+=chr(char)
 print(out)
