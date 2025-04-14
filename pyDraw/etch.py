2bbbeb6582edacbba63210baeca15a34a2d5e623
William Miller <Wmilleiv@gmail.com>
2025-04-14 12:02:38 -0400

    basic etch a sketch

--- pyDraw/etch.py
+++ pyDraw/etch.py
@@ -2,14 +2,35 @@ import turtle
 
 
 t=turtle.Turtle()
+t.color="purple"
+
 
 def goRight():
     t.setheading(0)
+    t.forward(10)
+
+def goUp():
+    t.setheading(90)
+    t.forward(10)
+
+def goLeft():
+    t.setheading(180)
+    t.forward(10)
+
+def goDown():
+    t.setheading(270)
     t.forward(10)
 
 while(True):
     x=input()
     if(x=='d'):
         goRight()
+    elif(x=='w'):
+        goUp()
+    elif(x=='a'):
+        goLeft()
+    elif(x=='s'):
+        goDown()
+
 turtle.mainloop()
 
