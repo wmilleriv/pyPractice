5d902412e6565690a2bcab11f3e3490e683828f3
William Miller <Wmilleiv@gmail.com>
2025-04-30 09:22:15 -0400

    koch fractals

---
+++ pyDraw/koch.py
@@ -0,0 +1,25 @@
+import turtle
+
+t=turtle.Turtle()
+
+def koch(length, level):
+    if level==0:
+        t.forward(length)
+        return
+    koch(length/3, level-1)
+    t.left(60)
+    koch(length/3, level-1)
+    t.right(120)
+    koch(length/3, level-1)
+    t.left(60)
+    koch(length/3, level-1)
+
+t.pencolor("blue")
+t.speed(0)
+t.up()
+t.setpos(-600,0)
+t.down()
+for i in range(3):
+    koch(600.0,4)
+    t.left(-120)
+turtle.done()
