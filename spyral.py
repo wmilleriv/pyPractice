5ae3d759126738c4e78b99ff092e51e261579628
William Miller <Wmilleiv@gmail.com>
2025-08-22 15:03:00 -0400

    move to center

--- spyral.py
+++ spyral.py
@@ -2,13 +2,13 @@ import turtle as t
 from random import random
 
 t.speed(10)
-t.goto(-500.00,50.00)
+t.goto(0,0)
 t.clear()
 for i in range(100):
     steps=int(random()*1000)
     for c in ('blue', 'red', 'green'):
         t.color(c)
-        t.forward(random()*1000)
+        t.forward(random()*100)
         t.right(60)
 
 t.screen.mainloop()
