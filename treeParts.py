391650da27d05557fbf8d3c6cb3f7b21c88c0545
William Miller <wmilleiv@gmail.com>
2025-11-24 12:05:53 -0500

    tree trunk

---
+++ treeParts.py
@@ -0,0 +1,13 @@
+import turtle
+
+t=turtle.Turtle()
+
+def drawTrunk(t, width, height):
+    t.fwd(width)
+    t.right(90)
+    t.fwd(height)
+    t.right(90)
+    t.fwd(width)
+    t.right(90)
+    t.fwd(height)
+
