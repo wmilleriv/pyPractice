34f917053d7bf2ef8ad8ce5f881d765bfcb2a6c8
William Miller <wmilleiv@gmail.com>
2025-12-11 11:01:10 -0500

    dragon work

--- tree/dragon.py
+++ tree/dragon.py
@@ -1,8 +1,16 @@
-import shapes
+import treeParts
 import turtle
 
 t=turtle.Turtle()
+t.up()
 
-
+t.setpos(-100,0)
+treeParts.drawTrunk(t,200,100,"green")
+t.setheading(0)
+t.setpos(100,20)
+treeParts.drawTrunk(t,75,50,"green")
+t.setpos(-90,-20)
+t.setheading(225)
+treeParts.drawTrunk(t,100,20,"green")
 
 turtle.done()
